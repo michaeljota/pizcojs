d9300f7e749c3f9f0985954e20241f622f3bb1f5
Michael De Abreu <michaeljota@outlook.com>
2016-01-08 23:20:13 -0430

    El menú de la barra se modifico para mostrar información de la aplicación. (fix: #8, ref: #23)

--- client/components/toolbar/toolbar.controller.js
+++ client/components/toolbar/toolbar.controller.js
@@ -3,33 +3,15 @@
 angular.module('tesisApp')
   .controller('ToolbarCtrl', function ($scope, $mdDialog, $mdSidenav) {
 
-        var originatorEv;
-
-
         $scope.openMenu = function($mdOpenMenu, ev) {
-            originatorEv = ev;
             $mdOpenMenu(ev);
         };
 
-        /*$scope.redial = function() {
-            $mdDialog.show(
-                $mdDialog.alert()
-                    .targetEvent(originatorEv)
-                    .clickOutsideToClose(true)
-                    .parent('body')
-                    .title('Suddenly, a redial')
-                    .textContent('You just called a friend; who told you the most amazing story. Have a cookie!')
-                    .ok('That was easy')
-            );
-            originatorEv = null;
-        };*/
-
-        $scope.download = function () {
-            console.log('Downloading');
+        $scope.showAppInfo = function () {
+            
         };
 
         $scope.openSidenav = function () {
-            console.log('Opening sidenav');
             $mdSidenav('left')
                 .toggle();
         };
