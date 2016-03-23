b22ed0264c25d0a1916f6a29c445967a003cd5d9
Michael De Abreu <michaeljota@outlook.com>
2016-03-22 21:44:12 -0430

    Creada una función para limpiar el canvas dentro del manajador de la pizarra.

--- client/components/services/whiteboardRenderer/whiteboardRenderer.service.js
+++ client/components/services/whiteboardRenderer/whiteboardRenderer.service.js
@@ -9,10 +9,14 @@
     var _shapes = [];
     var _intervalId;
 
+    function clearCanvas() {
+      canvas.context.fillStyle="#FFFFFF";
+      canvas.context.fillRect(0, 0, canvas.getSize().width, canvas.getSize().height);
+    }
+
     function drawShapes() {
       if(_shapes && _shapes.length > 0) {
-        canvas.context.fillStyle="#FFFFFF";
-        canvas.context.fillRect(0, 0, canvas.getSize().width, canvas.getSize().height);
+        clearCanvas();
         _shapes.forEach(function (shape) {
           if (!shape.points || shape.points.length === 0) {
             return;
